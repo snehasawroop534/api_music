23698cd5c840309dbcf3f1325cb1a2fdbc92af2b
Sneha kumari01 <snehasawroop534@gmail.com>
2025-10-29 20:58:51 +0530

    post_method_api_created

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ app.get("/api/user",(request, response)=>{
 
 });
 
+
+app.post("/api/user",(request, response)=>{
+    response.status(200).json({name: "Sneha", email: "sneha@gmail.com", id: 1});
+
+});
+
+
 app.listen(4000, (error)=>{
     if(error) console.log("Error "+ error);
     console.log("Server is running on port 4000");
 })
+
