affff5f6f680837e0ba8d273e5808c6c37e5c9d1
Sneha kumari01 <snehasawroop534@gmail.com>
2025-10-29 20:30:50 +0530

    project is created all

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/user",(request, response)=>{
+    response.status(200).json({name: "Sneha", email: "sneha@gmail.com", id: 1});
+
+});
+
+app.listen(4000, (error)=>{
+    if(error) console.log("Error "+ error);
+    console.log("Server is running on port 4000");
+})
