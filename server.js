fd457664fc1ea0f5eb824b38484bb16e0811b700
Sneha kumari01 <snehasawroop534@gmail.com>
2025-11-09 00:07:39 +0530

    changing post method

--- server.js
+++ server.js
@@ -9,15 +9,53 @@ app.get("/api/project",async(request, response)=>{
 });
 
 
-app.post("/api/user/register",(request, response)=>{
+app.post("/api/project/register",async(request,response)=>{
 
-    response.status(200).json({name: "Sneha", email: "sneha@gmail.com", id: 1});
+    const name = request.body.name;
+    const email = request.body.email;
+    const password = request.body.password;
+    const hashPassword = await bcrypt.hash(password, 10);
+  
 
+    db.query("INSERT INTO users(name, email , password) VALUES (? , ? , ?)",[name, email, hashPassword],(error, result)=>{
+        if(error) return response.status(500).json({message : "Server internal error"});
+        response.status(201).json({id: result.insertId,name : name, email : email});
+     } );
 });
+
+
+app.post("/api/project/login", async (request,response)=>{
+
+    const email = request.body.email;
+    const password = request.body.password;
+   
+
+    db.query("SELECT * FROM users WHERE email=?",[email],async(error, result)=>{
+        if(error) return response.status(500).json({message : "Server internal error"});
+        const dbPassword = result[0].password;
+          const name = result[0].name;
+          const email = result[0].email;
+        const isPasswordSame = await bcrypt.compare(password,dbPassword);
+        if(isPasswordSame){
+             const secretkey ="sddfgh56ds";
+            const token = jwt.sign({name:name, email:email}, secretkey,{expiresIn:"1h"} ); //token
+             response.status(200).json({message: "Login Success", token: token})
+        }
+        else{
+
+
+           response.status(200).json({message: "Login Failed"}) 
+        }
+        
+     } );
+
+    })
+
+
 
 
 app.listen(4003, (error)=>{
     if(error) console.log("Error "+ error);
-    console.log("Server is running on port 4000");
+    console.log("Server is running on port 4003");
 })
 
